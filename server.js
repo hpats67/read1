d83b02e359dbf4d93e2c8edb10dd22083f80b2f5
Steven Jackson <Steven Jackson>
2016-10-27 14:31:15 -0700

    next typing

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 var http = require('http');
+var url = require('url');
 
-http.createServer(function(request, response){
-    response.writeHead(200, {"content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-}).listen(8888);
+function start() {
+    function onRequest(request, response){
+        var pathname = url.parse(request.url).pathname;
+        console.log('Request for ' + pathname + ' received.');
+
+        route(handle, pathname);
+
+        response.writeHead(200, {"content-Type": "text/plain"});
+        response.write("Hello World");
+        response.end();
+    }
+
+http.createServer(onRequest).listen(8888);
+console.log('Sever has started.')
+
+}
+
+exports.start = start;
