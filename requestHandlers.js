af94dac544b1ff2ec6f28b66fd4df0016d5c6554
Steven Jackson <Steven Jackson>
2016-10-27 15:56:30 -0700

    finished reading and code

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,23 +1,55 @@
-var exec = require('child_process').exec
+var querystring = require('querystring'),
+    fs = require('fs');
+    formidable = require('formidable');
 
 function start(response) {
     console.log("Request handler 'start' was called.");
-    
-    exec('find /', 
-    { timeout: 10000, maxbuffer: 20000*1024 },
-    function(error, stdout, stderr){
+
+    var body = '<html>'+
+                '<head>'+
+                '<meta http-equiv="Content-Type" content="text/html; '+
+                'charset=UTF-8" />'+
+                '</head>'+
+                '<body>'+
+                '<form action="/upload" method="post">'+
+                '<textarea name="text" rows="20" cols="60"></textarea>'+
+                '<input type="submit" value="Submit text" />'+
+                '</form>'+
+                '</body>'+
+                '</html>';
+
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.write(stdout);
         response.end();
-    })
 };
 
 function upload() {
     console.log("Request handler 'upload' was called.");
+    
+    var form = new formidable.IncomingForm();
+    console.log('about to parse');
+    form.parse(request, function(error, fields, files) {
+        console.log('parsing done');
+
+        fs.rename(files.upload.path, '/tmp/test.png', function(error) {
+            if (error) {
+                fs.unlink('/tmp/test.png');
+                fs.rename(files.upload.path, 'tmp/test.png');
+            }
+        })
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hello World');
+    response.write('You\'ve sent the text '+
+    querystring.parse(postData).text);
     response.end();
+    });
 };
+
+function show(response) {
+    console.log('Request Handler \'show\' was called.');
+    response.writeHead(200, {'Content-Type': 'image/png'});
+    fs.createReadStream('/tmp/test.png').pipe(response);
+}
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
